5febf9902a6156943e44e1ac6c510e389c7c870d
shreyas134 <shreyas@email.sc.edu>
2026-07-14 16:42:57 -0400

    tmp

---
+++ Mathlib/ProjectCoherent/WeakCoherent.lean
@@ -0,0 +1,7 @@
+module
+public import Mathlib.AlgebraicGeometry.Noetherian
+public import Mathlib.Algebra.Category.ModuleCat.Sheaf.Quasicoherent
+
+open AlgebraicGeometry
+
+variable (X : Scheme) [IsLocallyNoetherian X]
