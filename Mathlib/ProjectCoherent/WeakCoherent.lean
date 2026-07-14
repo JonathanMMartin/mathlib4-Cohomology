fcf39b405df0b9d0667d9f82ea47ccdc255e4adb
shreyas134 <shreyas@email.sc.edu>
2026-07-14 17:41:23 -0400

    small change to WeakCoherent.lean

--- Mathlib/ProjectCoherent/WeakCoherent.lean
+++ Mathlib/ProjectCoherent/WeakCoherent.lean
@@ -1,4 +1,5 @@
 module
+
 public import Mathlib.AlgebraicGeometry.Noetherian
 public import Mathlib.Algebra.Category.ModuleCat.Sheaf.Quasicoherent
 public import Mathlib.AlgebraicGeometry.Modules.Presheaf
@@ -7,6 +8,8 @@ open AlgebraicGeometry
 
 variable (X : Scheme) [IsLocallyNoetherian X]
   (M : SheafOfModules X.ringCatSheaf)
+
+--Weak definition of Coherent sheaf assuming X is a locally noetherian scheme
 
 structure WeakCoherent : Prop where
   quasicoherent : M.IsQuasicoherent
